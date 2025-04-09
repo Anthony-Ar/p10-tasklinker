cb5be0d71ee268292aabeb08a4a7aed6d5555969
Anthony <anthony.arrighi@orange.fr>
2025-04-09 16:10:51 +0200

    feat: Ajout du champ "rôles" dans la modification utilisateur

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -4,17 +4,20 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Validator\Constraints\Regex;
 
 class UserType extends AbstractType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options): void
+    public function buildForm(FormBuilderInterface $builder, array $options) : void
     {
         $builder
             ->add('email', EmailType::class, [
@@ -62,10 +65,23 @@ class UserType extends AbstractType
                     )
                 ]
             ])
-        ;
+            ->add('roles', ChoiceType::class, [
+                'label' => 'Rôle',
+                'choices' => [
+                    'Collaborateur' => 'ROLE_USER',
+                    'Chef de projet' => 'ROLE_ADMIN',
+                ],
+                'mapped' => false
+            ]);
+
+        $builder->addEventListener(FormEvents::SUBMIT, function(FormEvent $event) {
+            $user = $event->getData();
+            $role = $event->getForm()->get('roles')->getData();
+            $user->setRoles([$role]);
+        });
     }
 
-    public function configureOptions(OptionsResolver $resolver): void
+    public function configureOptions(OptionsResolver $resolver) : void
     {
         $resolver->setDefaults([
             'data_class' => User::class,
